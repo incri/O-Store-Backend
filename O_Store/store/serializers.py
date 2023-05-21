30f176257e70fc6573dab73750593b138142d869
incri <paudelbaibhav0@gmail.com>
2023-05-21 18:27:43 +0545

    collection-list, collection-detail API created with GET, PUT, DELETE HTTPRequest feature

--- O_Store/store/serializers.py
+++ O_Store/store/serializers.py
@@ -2,19 +2,21 @@ from rest_framework import serializers
 from .models import Product, Collection
 from decimal import Decimal
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length =255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', 'products_count']
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length = 225)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    products_count = serializers.IntegerField()
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title','description', 'price','price_with_tax', 'collection', 'inventory', 'slug']
+     
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name= 'collection-detail'
-    )
+
 
     def calculate_tax(self, product: Product):
         return product.price* Decimal(1.1)
