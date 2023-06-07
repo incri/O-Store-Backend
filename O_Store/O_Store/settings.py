a641cb10d95ddccb34fc8f3573c24d1d81a6ab7b
incri <paudelbaibhav0@gmail.com>
2023-06-07 18:13:29 +0545

    changed access token time limit and reconfigured UserSerializer to include firstname and lastnae to /me endpoint after logging in

--- O_Store/O_Store/settings.py
+++ O_Store/O_Store/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -148,10 +149,13 @@ REST_FRAMEWORK = {
     ),
 }
 
-SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("JWT",),
-}
+SIMPLE_JWT = {"AUTH_HEADER_TYPES": ("JWT",), "ACCESS_TOKEN_LIFETIME": timedelta(days=1)}
 
 AUTH_USER_MODEL = "core.User"
 
-DJOSER = {"SERIALIZERS": {"user_create": "core.serializers.UserCreateSerializer"}}
+DJOSER = {
+    "SERIALIZERS": {
+        "user_create": "core.serializers.UserCreateSerializer",
+        "current_user": "core.serializers.UserSerializer",
+    }
+}
