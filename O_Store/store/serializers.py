7134a833eb7ef4338302a53a64a5c0dd13b17429
incri <paudelbaibhav0@gmail.com>
2023-06-07 18:50:01 +0545

    completed user personal profile page endpoint

--- O_Store/store/serializers.py
+++ O_Store/store/serializers.py
@@ -108,7 +108,7 @@ class CartUpdateSerializer(serializers.ModelSerializer):
 
 
 class CustomerSerializer(serializers.ModelSerializer):
-    user_id = serializers.IntegerField()
+    user_id = serializers.IntegerField(read_only = True)
     class Meta:
         model = Customer
         fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
