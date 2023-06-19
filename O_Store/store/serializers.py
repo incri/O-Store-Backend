50b046730d32d6386394ddd660d3dd8bd2f93dde
incri <paudelbaibhav0@gmail.com>
2023-06-19 15:00:12 +0545

    retrurning order items after oder completion

--- O_Store/store/serializers.py
+++ O_Store/store/serializers.py
@@ -168,3 +168,4 @@ class CreateOrderSerializer(serializers.Serializer):
 
             OrderItem.objects.bulk_create(order_items)
             Cart.objects.filter(pk=cart_id).delete()
+            return order
