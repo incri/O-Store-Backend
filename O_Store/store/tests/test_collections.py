dbff289c8964c7e7d81c3246e0098706eb8dc602
incri <paudelbaibhav0@gmail.com>
2023-07-16 22:17:07 +0545

    test: collection create: is_valid_data, is_invalid_data, is_auth_user, is_admin

--- O_Store/store/tests/test_collections.py
+++ O_Store/store/tests/test_collections.py
@@ -1,18 +1,44 @@
+from django.contrib.auth.models import User
+
 from rest_framework.test import APIClient
 from rest_framework import status
 import pytest
 
+
 @pytest.mark.django_db
 class TestCreateCollection:
     def test_if_user_is_anonymous_return_401(self):
-        #AAA (Arrange, Act, Assert)
+        # AAA (Arrange, Act, Assert)
 
-        #Arranger
+        # Arranger
 
-        #Act
+        # Act
         client = APIClient()
-        response = client.post('/store/collections/', {'title':'a'})
-        
-        #Assert
+        response = client.post("/store/collections/", {"title": "a"})
+
+        # Assert
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_return_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post("/store/collections/", {"title": "a"})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_return_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post("/store/collections/", {"title": ""})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data["title"] is not None
+
+    def test_if_data_is_valid_return_201(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post("/store/collections/", {"title": "a"})
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data["id"] > 0
